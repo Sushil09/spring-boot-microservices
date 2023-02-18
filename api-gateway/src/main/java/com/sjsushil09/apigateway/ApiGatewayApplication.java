173c87346cff8e8d7fcd07bd6968cd40de012c42
Sushil Jaiswal <sushil.jaiswal@oracle.com>
2023-02-18 22:56:04 +0530

    Introducing API-GATEWAY and configuring routes.

---
+++ api-gateway/src/main/java/com/sjsushil09/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.sjsushil09.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
---
+++ api-gateway/src/test/java/com/sjsushil09/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sjsushil09.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
